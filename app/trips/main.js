1af9204a9a107c138978104bae6777a30f65353c
glitchoff <194670407+glitchoff@users.noreply.github.com>
2026-02-05 15:07:10 +0530

    i changed the files

---
+++ app/trips/main.js
@@ -0,0 +1,7 @@
+export const Main = () => {
+    return (
+        <>
+MAIN WORLD
+        </>
+    )
+}
---
+++ app/trips/page.js
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import { Main } from "./main";
+
+export default function Home() {
+  return (
+   
+
+    <>
+    <Main />
+    
+    </>
+  );
+}
